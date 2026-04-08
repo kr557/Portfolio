1dadba097c66b4142ae48a3a971d88e7e69cbae1
krishan joshi <kissujoshi9950@gmail.com>
2026-04-08 14:50:08 +0530

    Krishan_Joshi_Resume

--- script.js
+++ script.js
@@ -312,7 +312,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const heroButtons2 = document.querySelector('#home .flex.flex-wrap.items-center');
   if (heroButtons2) {
     const resumeBtn = document.createElement('a');
-    resumeBtn.href = './Krishan_joshi_Ui and Ux Designer.pdf';
+    resumeBtn.href = './Krishan_Joshi_Resume.pdf';
     resumeBtn.download = 'Krishan_Joshi_Resume.pdf';
     resumeBtn.id = 'resume-btn';
     resumeBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="7 10 12 15 17 10"/><line x1="12" y1="15" x2="12" y2="3"/></svg> Resume`;
